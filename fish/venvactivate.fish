9e57450b0923feebd5e4465d2ed098c44b48fff2
raflhussain <raflkussayhussain@gmail.com>
2018-02-27 20:46:17 -0500

    powerline-shell json

--- fish/venvactivate.fish
+++ fish/venvactivate.fish
@@ -33,7 +33,7 @@ end
 # unset irrelavent variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/raflhussain/.venvprojects/stocks"
+set -gx VIRTUAL_ENV "/home/raflhussain/.venvprojects/$argv"
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
 
